d104ccf08fefae7dabf6499457dc24279c7cdc42
Luis Rangel <luis.rangel@telus.com>
2023-08-17 20:08:54 -0600

    set actor

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log("Hello World! Luis Rangel");
+console.log(`Software Developer: Luis Rangel`);
+console.log(`Hello World!!! ${process.env.USER}`);
+console.log(`${process.env.GHA_USERNAME}`);
